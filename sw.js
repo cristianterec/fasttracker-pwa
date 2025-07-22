b51613286db3be5aa1be4a4c316f1465329ba1be
cristianterec <cristianterec97@gmail.com>
2025-07-22 14:11:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,42 +3,43 @@ const CACHE_NAME = 'fasttrackers-v1.0.0';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
   '/app.js',
+  '/styles.css',
   '/manifest.json',
   '/my_icon.png',
   '/my_icon_192.png'
 ];
 
-// Install event
-self.addEventListener('install', event => {
+// Install event - cache resources
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
+      .then((cache) => {
+        console.log('FastTrackers cache opened');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Fetch event
-self.addEventListener('fetch', event => {
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then((response) => {
         // Return cached version or fetch from network
         return response || fetch(event.request);
       })
   );
 });
 
-// Activate event
-self.addEventListener('activate', event => {
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
